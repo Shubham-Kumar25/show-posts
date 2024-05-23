5594df5b95034557cc856aeb6c07c26764e9353e
Shubham Kumar <ishubhamjnv@gmail.com>
2024-05-23 17:49:35 +0530

    filter by author

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -10,8 +10,9 @@ function HomePage() {
   const posts = useSelector((state) => state.posts);
   const users = useSelector((state) => state.users);
   const [loading, setLoading] = useState(true);
-
   const [currentPage, setCurrentPage] = useState(1);
+  const [selectedAuthor, setSelectedAuthor] = useState("");
+
   const postsPerPage = 10;
 
   useEffect(() => {
@@ -41,6 +42,16 @@ function HomePage() {
     }
   }, [posts.length, fetchUsers]);
 
+  const handleAuthorChange = (e) => {
+    setSelectedAuthor(e.target.value);
+    console.log(selectedAuthor);
+    setCurrentPage(1);
+  };
+
+  const filteredPosts = selectedAuthor
+    ? posts.filter((post) => post.userId === Number(selectedAuthor))
+    : posts;
+
   useEffect(() => {
     window.scrollTo({
       top: 0,
@@ -51,13 +62,13 @@ function HomePage() {
   const indexOfLastPost = currentPage * postsPerPage;
   const indexOfFirstPost = indexOfLastPost - postsPerPage;
   const currentPosts = useMemo(
-    () => posts.slice(indexOfFirstPost, indexOfLastPost),
-    [posts, indexOfFirstPost, indexOfLastPost]
+    () => filteredPosts.slice(indexOfFirstPost, indexOfLastPost),
+    [posts, indexOfFirstPost, indexOfLastPost, filteredPosts]
   );
 
   const pageNumbers = useMemo(() => {
     const numbers = [];
-    for (let i = 1; i <= Math.ceil(posts.length / postsPerPage); i++) {
+    for (let i = 1; i <= Math.ceil(filteredPosts.length / postsPerPage); i++) {
       numbers.push(i);
     }
     return numbers;
@@ -71,34 +82,51 @@ function HomePage() {
       {loading ? (
         <Loader />
       ) : (
-        <div className="grid gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
-          {currentPosts.map((post) => (
-            <div
-              key={post.id}
-              className="overflow-hidden transition duration-300 bg-gray-200 border border-gray-700 shadow-xl rounded-3xl hover:shadow-lg"
+        <>
+          <div>
+            <select
+              value={selectedAuthor}
+              onChange={handleAuthorChange}
+              id="author"
+              className="w-full p-2 mb-4 rounded-full"
             >
-              <Link to={`/post/${post.id}`}>
-                <div className="px-6 py-4">
-                  <div className="mb-2 text-xl font-semibold text-violet-800">
-                    <span className="font-bold text-green-800">Title:</span>{" "}
-                    {post.title}
-                  </div>
-                  <p className="text-base text-gray-700">
-                    <span className="font-semibold text-green-800">
-                      Content:
-                    </span>{" "}
-                    {post.body}
-                  </p>
-                  {users[post.userId] && (
-                    <p className="mt-4 text-base font-semibold text-red-500">
-                      <span>Author:</span> {users[post.userId].name}
+              <option value="">All Authors</option>
+              {Object.values(users).map((user) => (
+                <option key={user.id} value={user.id}>
+                  {user.name}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="grid grid-cols-1 gap-4">
+            {currentPosts.map((post) => (
+              <div
+                key={post.id}
+                className="overflow-hidden transition duration-300 bg-gray-200 border border-gray-700 shadow-xl rounded-3xl hover:shadow-lg"
+              >
+                <Link to={`/post/${post.id}`}>
+                  <div className="px-6 py-4">
+                    <div className="mb-2 text-xl font-semibold text-violet-800">
+                      <span className="font-bold text-green-800">Title:</span>{" "}
+                      {post.title}
+                    </div>
+                    <p className="text-base text-gray-700">
+                      <span className="font-semibold text-green-800">
+                        Content:
+                      </span>{" "}
+                      {post.body}
                     </p>
-                  )}
-                </div>
-              </Link>
-            </div>
-          ))}
-        </div>
+                    {users[post.userId] && (
+                      <p className="mt-4 text-base font-semibold text-red-500">
+                        <span>Author:</span> {users[post.userId].name}
+                      </p>
+                    )}
+                  </div>
+                </Link>
+              </div>
+            ))}
+          </div>
+        </>
       )}
       <div className="flex justify-center mt-4">
         <nav>
